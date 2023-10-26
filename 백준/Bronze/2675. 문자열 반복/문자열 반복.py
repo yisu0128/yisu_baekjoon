f72aa95fe9b38194bc6b6b097d92bc8e9da6a8bd
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2023-10-26 10:39:53 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,7 @@
+
+for i in range(int(input())):
+    r,s = map(str, input().split())
+    r = int(r)
+    for i in range(len(s)):
+        print(s[i]*r, end = '')      
+    print()                                                                                                                                                                              

b279e3933be2643f65f0928eab134d4e20a931d8
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2023-10-28 09:12:03 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,9 @@
+a,b,c,d,e,f = map(int, input().split())
+a1,b1,c1,d1,e1,f1 = 1,1,2,2,2,8
+
+print(a1-a, end = " ")
+print(b1-b, end = " ")
+print(c1-c, end = " ")
+print(d1-d, end = " ")
+print(e1-e, end = " ")
+print(f1-f, end = " ")

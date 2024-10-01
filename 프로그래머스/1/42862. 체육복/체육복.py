51b473ba1288c542fbdb2d258fdfb4500c0d5026
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-10-01 12:31:04 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,13 @@
+from collections import *
+def solution(n, lost, reserve):
+    answer = 0 
+    lost_set = set(lost)- set(reserve)
+    reserve_set = set(reserve)-set(lost)
+        
+    for r in reserve_set:
+        if r-1 in lost_set:
+            lost_set.remove(r-1)
+        elif r+1 in lost_set:
+            lost_set.remove(r+1)
+    return n-len(lost_set)
+                

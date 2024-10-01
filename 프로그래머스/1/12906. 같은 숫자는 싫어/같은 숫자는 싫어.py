55837b0fc5e01586dec3b4a90493aef5003d49f4
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-10-01 16:33:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 104.49 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,10 @@
-from collections import *
+from collections import * 
 
 def solution(arr):
-    arr2 = deque()
-    arr2.append(arr[0])
+    fl = list()
+    fl.append(arr[0])
     for i in range(1,len(arr)):
-        if arr[i]!=arr[i-1]:
-            arr2.append(arr[i])
-    return list(arr2)
+        if arr[i] !=arr[i-1]:
+            fl.append(arr[i])
+    
+    return fl
